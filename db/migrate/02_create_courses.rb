8f7c8d3e32800dce35f7f11668255d9a5d9ed6ef
cccswann <cccswann@gmail.com>
2021-02-07 17:28:45 -0800

    Create courses migration

---
+++ db/migrate/02_create_courses.rb
@@ -0,0 +1,14 @@
+class CreateCourses < ActiveRecord::Migration
+    def change
+        create_table :courses do |t|
+            t.string :course_name
+            t.string :subject
+            t.string :status
+            t.string :url
+            t.integer :user_id
+
+            t.datetime :created_at
+            t.datetime :updated_at
+        end
+    end
+end
