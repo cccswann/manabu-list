61657d6033b361a36021b4fad471139387853ec8
cccswann <cccswann@gmail.com>
2021-02-07 18:30:21 -0800

    Add activerecord & sinatra env

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,19 @@
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler/setup'
-Bundler.require
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
 
 require_all 'app'
+#require './app/contollers/application_controller'
+
 require 'sinatra/activerecord'
 require 'sinatra'
+require 'open-uri'
+
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
 
