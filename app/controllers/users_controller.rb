6436ea5f2d9196acf1fb10e3cb5482f5b3717da6
cccswann <cccswann@gmail.com>
2021-02-11 18:02:58 -0800

    Create post functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    get '/signup' do
+        erb :"/users/signup"
+    end
+
+    post '/signup' do
+        user = User.create(params["user"])
+        if user.valid?
+            flash[:success] = "Successfully created account"
+            session["user_id"] = user.id
+            redirect "/courses"
+        else
+            flash[:error] = user.errors.full_messages.first
+            redirect '/signup'
+        end
+    end
 end
