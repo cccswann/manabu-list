28f366b8dd7ec5f19d5ad0a58d862522fcca2be0
cccswann <cccswann@gmail.com>
2021-02-11 22:13:51 -0800

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+    get '/login' do
+        if logged_in?
+            erb :"users/dashboard"
+        else
+            erb :"/sessions/login"
+        end
+    end
+
+    post '/login' do
+        user = User.find_by_email(params["user"]["email"])
+        if user && user.authenticate(params["user"]["password"])
+            session["user_id"] = user.id
+            flash[:success] = "Successfully logged in!"
+            redirect "/courses"
+        else
+            flash[:error] = "Invalid credentials"
+            redirect "/login"
+        end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/"
+    end
+
+
+end
