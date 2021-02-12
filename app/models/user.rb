7cba6144e83028df1f5b4141ff7169b7ba803c4b
cccswann <cccswann@gmail.com>
2021-02-11 18:06:58 -0800

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-    has_secure_password
     has_many :courses
-
+    has_secure_password
+    
+    validates :password, length: {in: 10..100}, confirmation: true
+    validates :username, uniqueness: true, length: {in: 5..30}, exclusion: {in: %w(admin superadmin user)}
+    validates :email, presence: true, uniqueness: true, format: {with:/\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
 end
