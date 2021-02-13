bf6358ec1b44a56d12883c44da0ed69129f9690f
cccswann <cccswann@gmail.com>
2021-02-13 10:13:38 -0800

    Update and create method
    Update current user method & add not the owner

--- app/Helpers/helpers.rb
+++ app/Helpers/helpers.rb
@@ -5,7 +5,14 @@ module Helpers
     end
 
     def current_user
-        User.find(session[:user_id])
+        @current_user ||= User.find(session[:user_id]) if session[user_id]
+    end
+
+    def not_the_owner?(obj)
+        if current_user != obj.user
+            flash[:error] = "You do not have permission for that page."
+            erb :"/users/dashboard"
+        end
     end
 
 end
