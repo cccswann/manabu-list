17092bff69e8e4ce4714a65693acf4cecd38d88c
cccswann <cccswann@gmail.com>
2021-02-14 18:04:27 -0800

    Add completed and total lectures attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2) do
     t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "completed_lectures"
+    t.integer "total_lectures"
   end
 
   create_table "users", force: :cascade do |t|
