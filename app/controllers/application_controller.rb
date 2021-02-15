238b37c034034f32fc2a8d43275f6ddc0eefec44
cccswann <cccswann@gmail.com>
2021-02-14 18:01:24 -0800

    Update application controller
    include Helpers and register Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < Sinatra::Base
+    include Helpers
 
     configure do 
         set :public_folder, 'public'
         set :views, 'app/views'
         enable :sessions
-        #set :session_secret 
+        set :session_secret, ENV['SESSION_KEY']
+        register Sinatra::Flash
     end
     
     get '/' do
-        puts "Hello World"
+        @users = User.all
+        if logged_in?
+            erb :"/users/dashboard"
+        else
+            erb :"/sessions/login"
+        end
     end
+
+    error Sinatra::NotFound do
+        erb :error
+    end
+
 end
